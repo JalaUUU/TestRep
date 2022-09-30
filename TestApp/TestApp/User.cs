d33912a7753459770da3064744f9ad104ecc7429
sa sa <betaw067@gmail.com>
2022-09-30 21:11:13 +1000

    FirstCommit

---
+++ TestApp/TestApp/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    internal class User
+    {
+        internal int Id { get; set; }
+        internal string Name { get; set; }
+        internal DateTime Birthdate { get; set; }
+        internal string Address { get; set; }
+    }
+}
